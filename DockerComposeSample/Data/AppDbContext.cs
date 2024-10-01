3e7b7ec6170a532a112e212a22e0fc0107ec930f
pourahmad <pourahmad@POURAHMAD-PC>
2024-10-01 09:51:01 +0330

    edit error connect to database

--- DockerComposeSample/Data/AppDbContext.cs
+++ DockerComposeSample/Data/AppDbContext.cs
@@ -1,13 +1,20 @@
 ﻿using DockerComposeSample.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace DockerComposeSample.Data;
 
-public class AppDbContext: DbContext
+public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-            
+        var databaseCreate = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+        if (databaseCreate != null)
+        {
+            if(!databaseCreate.CanConnect()) databaseCreate.Create();
+            if(!databaseCreate.HasTables()) databaseCreate.CreateTables();
+        }
     }
 
     public DbSet<User> Users { get; set; }
--- DockerComposeSample/Program.cs
+++ DockerComposeSample/Program.cs
@@ -9,8 +9,9 @@ builder.Services.AddDbContext<AppDbContext>(option =>
     string DB_HOST = Environment.GetEnvironmentVariable("DB_HOST");
     string DB_NAME = Environment.GetEnvironmentVariable("DB_NAME");
     string DB_SA_PASSWORD = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
-    var connectionstring = $"Data Source={DB_HOST};Initial Catalog={DB_NAME}; User ID=sa;Password={DB_SA_PASSWORD};TrustServerCertificate=True";
-    option.UseSqlServer(builder.Configuration.GetConnectionString(connectionstring));
+    var connectionstring = $"Data Source={DB_HOST};Initial Catalog={DB_NAME}; User ID=sa;Password={DB_SA_PASSWORD};TrustServerCertificate=True;";
+    option.UseSqlServer(connectionstring);
+    //option.UseSqlServer(builder.Configuration.GetConnectionString(connectionstring));
 });
 
 builder.Services.AddControllers();
