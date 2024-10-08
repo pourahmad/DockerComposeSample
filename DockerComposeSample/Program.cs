643df2c3ad3af2c27042fb372553d88a81741b33
pourahmad <pourahmad@POURAHMAD-PC>
2024-10-08 09:47:09 +0330

    add redis service

--- DockerComposeSample/Program.cs
+++ DockerComposeSample/Program.cs
@@ -1,8 +1,6 @@
 using DockerComposeSample.Data;
 using DockerComposeSample.Services;
-using DockerComposeSample.Services.Redis;
 using Microsoft.EntityFrameworkCore;
-using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,16 +15,11 @@ builder.Services.AddDbContext<AppDbContext>(option =>
     //option.UseSqlServer(builder.Configuration.GetConnectionString(connectionstring));
 });
 
-string DB_REDIS_HOST = Environment.GetEnvironmentVariable("DB_REDIS_HOST");
-builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(DB_REDIS_HOST));
-builder.Services.AddHttpClient();
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IUserService, UserServices>();
-builder.Services.AddScoped<IRedisCatchServices, RedisCatchServices>();
 
 var app = builder.Build();
 
